ba097a6c2ebf69617340483459c6b34104489451
Kirstyn Canull <kirstyncanull@kirstyns-air.mynetworksettings.com>
2024-01-07 21:03:05 -0500

    added photos, added javascript event, added css

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+function random(number) {
+    return Math.floor(Math.random() * number);
+  }
+  
+  function bgChange() {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    return rndCol;
+  }
+  
+  const container = document.querySelector("#container");
+  
+  container.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = bgChange();
+  });
 
-document.querySelector("body").appendChild(h2);
+  function random(number) {
+    return Math.floor(Math.random() * number);
+  }
+  
+  function bgChange() {
+    const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+    return rndCol;
+  }
+  
+  const container2 = document.querySelector("#container2");
+  
+  container.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = bgChange();
+  });
+  
